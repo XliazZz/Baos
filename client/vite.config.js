cc0ebb12d205f79f624687dca830e504a71811a6
Elias Martinez <99233887+XliazZz@users.noreply.github.com>
2025-04-25 22:14:09 -0300

    cambios

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss({
       config: {
         safelist: ['bg-indigo-50', 'bg-white', 'bg-gray-50', 'bg-gray-950', 'dark:bg-gray-950', 'dark:bg-indigo-950/70', 'dark:bg-slate-900'],
@@ -10,4 +12,5 @@ export default defineConfig({
       darkMode: 'class',
     }),
   ],
+  base: '/wow-bao/',
 })
